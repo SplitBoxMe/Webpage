6c026e3ad20837f280789debf01f2491ecd420cc
Markus Petrykowski <github@markus-petrykowski.de>
2015-10-03 22:19:09 +0200

    corrected mistake

--- js/googledriveconnector.js
+++ js/googledriveconnector.js
@@ -120,12 +120,10 @@ function addFile(filename, byteArray){
                 'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
             },
             'body': multipartRequestBody});
-        if (!callback) {
-            callback = function(file) {
-                console.log(file)
-            };
-        }
-        request.execute(callback);
+
+        request.execute(function(file){
+            console.log(file)
+        });
     }
 }
 
