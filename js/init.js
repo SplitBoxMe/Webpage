195d24676cb1ae5fab764361bec2fd5c9fd83771
Stephan Schultz <register@steppschuh.net>
2015-10-03 07:40:28 +0200

    Added upload progress modal

--- js/init.js
+++ js/init.js
@@ -11,6 +11,53 @@ initDropZone();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+function processFile() {  
+  $('#modalUpload').openModal();
+  setUploadStatus("Preparing file", 0);
+  //$('#modalUpload').closeModal();
+}
+
+function setUploadStatus(heading, percentage) {
+  var uploadHeader = document.getElementById("uploadHeader");
+  var uploadDescription = document.getElementById("uploadDescription");
+
+  if (heading != null) {
+    uploadHeader.innerHTML = heading;
+  }
+  uploadDescription.innerHTML = "SplitBox is processing your file, this may takes a few seconds. " + percentage + "% done."
+
+  var container = document.getElementById("uploadAnimationContainer");
+  var leftBox = document.getElementById("boxLeft");
+  var rightBox = document.getElementById("boxRight");
+  
+  container.percentage = percentage;
+
+  // resize box
+  leftBox.style.height = container.offsetHeight + "px";
+  leftBox.style.width = leftBox.style.height;
+
+  rightBox.style.height = leftBox.style.height;
+  rightBox.style.width = leftBox.style.width;
+
+  // center box
+  var maximumOffset = (leftBox.offsetWidth / 12);
+  var centerOffset = ((container.offsetWidth / 2) - (leftBox.offsetWidth / 2));
+  var percentageOffset = Math.round((percentage * maximumOffset) / 100);
+  
+  leftBox.style.left = (centerOffset - percentageOffset) + "px";
+  rightBox.style.left = (centerOffset + percentageOffset) + "px";
+  rightBox.style.top = (percentageOffset / 2) + "px"
+
+  // rotate
+  var maximumDeg = 20;
+  var deg = Math.round((percentage * maximumDeg) / 100);
+  rightBox.style.webkitTransform = 'rotate('+deg+'deg)'; 
+  rightBox.style.mozTransform    = 'rotate('+deg+'deg)'; 
+  rightBox.style.msTransform     = 'rotate('+deg+'deg)'; 
+  rightBox.style.oTransform      = 'rotate('+deg+'deg)'; 
+  rightBox.style.transform       = 'rotate('+deg+'deg)'; 
+}
+
 function initDropZone() {
   console.log("Initializing drop zone");
 
