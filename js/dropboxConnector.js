452817d32169bf59d4aa2b42810f5e248535d159
Markus Petrykowski <github@markus-petrykowski.de>
2015-10-03 07:45:09 +0200

    finished dropbox interaction

--- js/dropboxConnector.js
+++ js/dropboxConnector.js
@@ -1,25 +1,21 @@
 var dropboxAppKey = "5tj76xeexmiy76h",
-    client = null
+    client = null,
+    dropboxIsAuthenticated = false
 
-function initDropboxClient(){
-    client = new Dropbox.Client({ key: dropboxAppKey , token: "oxfXOJCMc9cAAAAAAAAYsqNYLaHpUE5ogP0Nw1-YbnPFsoaGgMwUOcbMGtxoJen0"});
+function initDropboxClient(token){
+    client = new Dropbox.Client({ key: dropboxAppKey , token: token});
     client.authenticate(function (err, client) {
         if(err){
             console.log(err)
             return false
         }else{
-            console.log("Successfully authenticated")
+            dropboxIsAuthenticated = true
             return true
         }
     });
 }
-initDropboxClient()
 
-function getCurrentToken(){
-    //Get token from cookie if it exists
-}
-
-function authorize(){
+function authorizeWithDropbox(){
     var redirect = "https://splitbox.me"
     window.location.replace( "https://www.dropbox.com/1/oauth2/authorize?client_id=" + dropboxAppKey + "&response_type=token&redirect_uri=" + redirect + "&state=dropboxLogin" )
 }
@@ -36,13 +32,35 @@ function getFile(filename){
     })
 }
 
-function writeFile(filename, data){
+function writeFileToDropbox(filename, data){
     client.readFile("/"+filename, data, function(err, content){
     })
 }
 
 function onDropboxRedirect(token){
-    localStorage.dropboxToken =  token
+    window.localStorage.setItem("dropboxToken", token)
+    initDropboxClient(token)
+    window.location.hash = ""
 }
 
-console.log(window.location.hash)
+function hashToDict(hash){
+    var dict = {}
+    hash = hash.replace("#", "")
+    if(hash.length > 0){
+        hash = hash.split("&")
+        for(var i=0; i<hash.length; i++){
+            var pair = hash[i].split("=")
+            dict[pair[0]] = pair[1]
+        }
+    }
+    return dict
+}
+
+var hashDict = hashToDict(window.location.hash)
+if( Object.keys(hashDict).length > 0 ){
+    if("state" in hashDict && hashDict["state"] == "dropboxLogin"){
+        onDropboxRedirect(hashDict["access_token"])
+    }
+}else if( window.localStorage.getItem("dropboxToken") != null ){
+    initDropboxClient(window.localStorage.getItem("dropboxToken"))
+}
