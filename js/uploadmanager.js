d27b2fefcf9e773fae5978354f0c89102157dffa
Carl Ambroselli <git@carl-ambroselli.de>
2015-10-04 00:56:55 +0200

    Different file names for each box

--- js/uploadmanager.js
+++ js/uploadmanager.js
@@ -3,9 +3,13 @@ function handleFileSelect(files) {
   $('#modalUpload').openModal();
   uploadStarted("Encrypting file", true);
 
+  function tmpName() {
+    return  Math.random().toString(36).substring(7) +  Math.random().toString(36).substring(7) +  Math.random().toString(36).substring(7)
+  }
+
   window.setTimeout(function() {
     var name = files[0].name
-    var tmpName = Math.random().toString(36).substring(7) +  Math.random().toString(36).substring(7) +  Math.random().toString(36).substring(7);
+    var tmpName =;
     for (var i = 0, f; f = files[i]; i++) {
       var reader = new FileReader();
       // Closure to capture the file information.
@@ -43,32 +47,32 @@ function handleFileSelect(files) {
           }
 
           if(googledriveIsAuthenticated && dropboxIsAuthenticated) {
-            uploadFileToGoogleDrive(tmpName, splitfile.key).then(function (result) {
+            uploadFileToGoogleDrive(tmpName(), splitfile.key).then(function (result) {
               links.key = result
               showLink()
             })
 
-            writeFileToDropbox(tmpName, splitfile.cipher).then(function(cipherResult) {
+            writeFileToDropbox(tmpName(), splitfile.cipher).then(function(cipherResult) {
               links.cipher = cipherResult
               showLink()
             })
           } else if(googledriveIsAuthenticated && onedriveIsAuthenticated) {
-            uploadFileToGoogleDrive(tmpName, splitfile.key).then(function (result) {
+            uploadFileToGoogleDrive(tmpName(), splitfile.key).then(function (result) {
               links.key = result
               showLink()
             })
 
-            uploadFileToOneDrive(tmpName, splitfile.cipher, function(cipherResult) {
+            uploadFileToOneDrive(tmpName(), splitfile.cipher, function(cipherResult) {
               links.cipher = cipherResult
               showLink()
             })
           } else {
-            writeFileToDropbox(tmpName, splitfile.key).then(function (result) {
+            writeFileToDropbox(tmpName(), splitfile.key).then(function (result) {
               links.key = result
               showLink()
             })
 
-            uploadFileToOneDrive(tmpName, splitfile.cipher, function(cipherResult) {
+            uploadFileToOneDrive(tmpName(), splitfile.cipher, function(cipherResult) {
               links.cipher = cipherResult
               showLink()
             })
