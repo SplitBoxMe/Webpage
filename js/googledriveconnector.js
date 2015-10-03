33a4dbc046d25c672c2a47e00b87e270e6ec173f
Markus Petrykowski <github@markus-petrykowski.de>
2015-10-03 13:45:06 +0200

    added corect client id

--- js/googledriveconnector.js
+++ js/googledriveconnector.js
@@ -1,6 +1,6 @@
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
-var CLIENT_ID = '554698242471-3th8kuior2siu7sdli3uii57oop42sd3.apps.googleusercontent.com';
+var CLIENT_ID = '51094403642-h70gvkengs2plg5tp5ah67jpn6ukhjq2.apps.googleusercontent.com';
 
 var SCOPES = ['https://www.googleapis.com/auth/drive.appfolder'];
 
