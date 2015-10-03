9152b7ecfbfcc3d5064a135708a7e0b58e3b0088
Carl Ambroselli <git@carl-ambroselli.de>
2015-10-03 17:17:22 +0200

    jQuery get binary data

--- js/downloadmanager.js
+++ js/downloadmanager.js
@@ -1,3 +1,45 @@
+$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+  // check for conditions and support for blob / arraybuffer response type
+  if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
+  {
+    return {
+      // create new XMLHttpRequest
+      send: function(headers, callback){
+        // setup all variables
+        var xhr = new XMLHttpRequest(),
+          url = options.url,
+          type = options.type,
+          async = options.async || true,
+        // blob or arraybuffer. Default is blob
+          dataType = options.responseType || "blob",
+          data = options.data || null,
+          username = options.username || null,
+          password = options.password || null;
+
+        xhr.addEventListener('load', function(){
+          var data = {};
+          data[options.dataType] = xhr.response;
+          // make callback and send data
+          callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+        });
+
+        xhr.open(type, url, async, username, password);
+
+        // setup custom headers
+        for (var i in headers ) {
+          xhr.setRequestHeader(i, headers[i] );
+        }
+
+        xhr.responseType = dataType;
+        xhr.send(data);
+      },
+      abort: function(){
+        jqXHR.abort();
+      }
+    };
+  }
+});
+
 function readFileParam() {
   var fileParam = getUrlParam("file");
   if (fileParam != null) {
@@ -26,22 +68,30 @@ function downloadFileParts(key, cipher) {
     key: ''
   }
 
-  $.get(key, function(result) {
-    console.log("Key download done")
-    splitfile.key = result
-    count++
-    callback(result)
-  }, function() {
-    console.log('error')
+  $.ajax({
+    url: key,
+    type: "GET",
+    dataType: "binary",
+    processData: false,
+    success: function(result){
+      console.log("Key download done")
+      splitfile.key = result
+      count++
+      callback(result)
+    }
   });
 
-  $.get(cipher, function(result) {
-    console.log("Cipher download done")
-    splitfile.cipher = result
-    count++
-    callback(result)
-  }, function() {
-    console.log('error')
+  $.ajax({
+    url: cipher,
+    type: "GET",
+    dataType: "binary",
+    processData: false,
+    success: function(result){
+      console.log("Cipher download done")
+      splitfile.cipher = result
+      count++
+      callback(result)
+    }
   });
 
   function callback() {
